4ff8b98dafbadbaa25e3745b716c04c497908237
Ayush Bikram <ghimireayush52@gmail.com>
2023-07-20 14:07:31 +0545

    hello

--- Login.php
+++ Login.php
@@ -1,3 +1,29 @@
+<?php
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $mysqli = require __DIR__ . "/db.php";
+    
+    $email = $mysqli->real_escape_string($_POST["email"]);
+    $sql = sprintf("SELECT * FROM user WHERE email = '%s'", $email);
+    
+    $result = $mysqli->query($sql);
+    
+    if ($result && $result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($_POST["password"], $user["password"])) {
+            session_start();
+            session_regenerate_id(); //Prevents session fixation attack
+            $_SESSION["user_id"] = $user["id"];
+            header("Location: index.html");
+            exit;
+        }
+    }
+    
+    $is_invalid = true;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,24 +35,20 @@
 <body>
   <div class="login-box">
     <h2>Login</h2>
-    <form>
+    <form method="post" action="">
       <div class="user-box">
-        <input type="text" name="" required="">
-        <label>Username</label>
+        <input type="text" name="email" required="">
+        <label>Email</label>
       </div>
       <div class="user-box">
-        <input type="password" name="" required="">
+        <input type="password" name="password" required="">
         <label>Password</label>
       </div>
-      <a href="#">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Submit
-      </a>
+      <button type="submit">Submit</button>
     </form>
+    <?php if ($is_invalid): ?>
+      <p>Invalid email or password</p>
+    <?php endif; ?>
   </div>
 </body>
 </html>
-
